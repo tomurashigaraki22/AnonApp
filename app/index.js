bd7dfc0642731e84cccaaa1ae4607da1b67e66f0
malikobansa <hackobansa@gmail.com>
2023-10-04 21:13:40 +0100

    Added new files and styles

--- app/index.js
+++ app/index.js
@@ -1,30 +1,87 @@
-import { View, Text, StyleSheet, Link } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { useRouter } from 'expo-router';
 
 const Index = () => {
+    const router = useRouter();
+
   return (
     <View style={styles.container}>
       <View style={styles.text}>
-         <Text>Welcome to Anon</Text>
-         <Text>Connect with other anonymously and share
+         <Text style={styles.texts}>Welcome to Anon</Text>
+         <Text style={styles.texts}>Connect with other anonymously and share
             your thoughts without revealing your identity.
             Anon is the perfect plaftorm to express yourself freely.
          </Text>
       </View>
 
-      <View style={styles.btn}>
-          <Link href="/login">Log In</Link>
-          <Link href="/signup">Sign Up</Link>
-      </View>
+      {/* <View style={styles.btn}>
+      <Button
+          title="Log In"
+          onPress={() => {
+            // Navigate to the login page
+            router.push('/login')  
+        }}
+        />
+        <Button
+          title="Sign Up"
+          onPress={() => {
+            // Navigate to the signup page
+           router.push('/signup')  
+        }}
+        /> 
+    </View>*/}
+
+     <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={() => {
+            // Navigate to the login page
+            router.push('/login')  
+        }}>
+            <Text style={styles.buttonText}>Log In</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => {
+            // Navigate to the login page
+            router.push('/signup')  
+        }}>
+            <Text style={styles.buttonText}>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
+    texts: {
+      textAlign:"center",
+      width:300,
+      marginLeft:50,
+      fontSize:20,
+    },
+    text: {
+      marginTop:250,
+    },
+    buttonContainer: {
+        width: "80%",
+        position: "absolute",
+        bottom: -100,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        left:40,
+      },
+      button: {
+        flex: 1,
         backgroundColor: "#000",
-        color:"#fff",
-    }
+        paddingVertical: 15,
+        paddingHorizontal: 30,
+        borderRadius: 10,
+        marginHorizontal: 5,
+      },
+      buttonText: {
+        color: "white",
+        fontSize: 18,
+        fontWeight: "bold",
+        textAlign: "center",
+      },
 })
 
 export default Index;
--- app/login.js
+++ app/login.js
@@ -1,12 +1,110 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { router } from 'expo-router';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleSubmit = () => {
+      // Handle your form submission logic here, e.g., validation or API calls
+      Alert.alert('Submitted', `Email: ${email}\nPassword: ${password}`);
+    };
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+     <View style={styles.textContainer}>
+    <Text style={styles.upper}>Anon Log In</Text>
+     <Text style={styles.texts}>Join an Exciting Social Experience.</Text>
+     </View>
+    <View style={styles.inputContainer}>
+     <Icon name="user" size={20} color="gray" style={styles.inputIcon} />
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+        keyboardType="text"
+        autoCapitalize="none"
+      />
+     </View>
+     <View style={styles.inputContainer}>
+     <Icon name="lock" size={20} color="gray" style={styles.inputIcon} />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry={true}
+      />
+     </View>
+
+     <TouchableOpacity style={styles.login} onPress={()=> { router.push("/home")}}> 
+     <Text style={styles.buttonText}>Log In</Text>
+     </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      padding: 16,
+    },
+    input: {
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 0,
+      marginBottom: 10,
+      paddingLeft: 8,
+      borderRadius: 10,
+      flex: 1,
+    },
+    inputContainer: {
+      flexDirection: 'row',
+      borderWidth: 1,
+      borderColor: '#ccc',
+      alignItems: 'center',
+      paddingLeft: 10,
+      marginBottom:20,
+      borderRadius:20,
+    },
+    inputIcon: {
+      marginRight: 10,
+    },
+    textContainer: {
+      marginBottom: 5,
+      // backgroundColor: '#6CB4EE',
+      width: 'auto',
+      height: "auto",
+    },
+    texts: {
+      fontSize:40,
+      color: "#000",
+      textAlign:'center',
+    },
+    upper: {
+        fontSize:20
+    },
+    login:{
+        width:200,
+        height:50,
+        backgroundColor: "#000",
+        color:"#fff",
+        borderRadius: 10,
+        marginLeft:80,
+    },
+    buttonText:{
+        position:"absolute",
+        top:10,
+        left:70,
+        color: "white",
+        fontSize: 18,
+        fontWeight: "bold",
+        textAlign: "center",
+    }
+    
+  });   
 
 export default Login
